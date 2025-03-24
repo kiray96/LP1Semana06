cfaf275f7d8b00becb761e6f2cfef0746ffb5eec
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-03-24 22:07:23 +0000

    Made the exercise 2, Chronos project

---
+++ Chronos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            crono1.Start();
+            Thread.Sleep(500);
+            crono2.Start();
+            Thread.Sleep(250);
+            crono1.Stop();
+            crono2.Stop();
+
+            TimeSpan ts1 = crono1.Elapsed;
+            TimeSpan ts2 = crono2.Elapsed;
+
+            Console.WriteLine($"{ts1.TotalSeconds:f4}");
+            Console.WriteLine($"{ts2.TotalSeconds:f4}");
+
+        }
+    }
+}

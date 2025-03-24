42c19cbacdb39fb37dacb2ba4fdc3b5f52edcf97
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-03-24 22:51:18 +0000

    Add commits to the code

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -6,18 +6,24 @@ namespace RandDice
     {
         private static void Main(string[] args)
         {
+            // Obter parametros da linha de comandos
             int n = Convert.ToInt32(args[0]);
             int s = Convert.ToInt32(args[1]);
 
+            // Criar nova instancia de random
             Random random = new Random(s);
 
+            // Iniciar a variável de contagem
             int count = 0;
 
+            //Loop n vezes 
             for(int i = 0; i < n; i++)
             {
+                // Soma um número random entre um e 6
                 count += random.Next(1, 7);
             }
 
+            // Imprimir o resultado 
             Console.WriteLine(count);
         }
     }

b80855ecbc0718a46bf88add9e99c348beb0ee5a
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-03-24 22:47:35 +0000

    Create and resolve the exercise 5, RandDice project

---
+++ RandDice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(args[0]);
+            int s = Convert.ToInt32(args[1]);
+
+            Random random = new Random(s);
+
+            int count = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                count += random.Next(1, 7);
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}

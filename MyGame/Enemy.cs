b85c2011ade7f76a9e80fcece49574f7686d0278
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-03-25 20:04:06 +0000

    Add the constructor in Enemy class

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -4,6 +4,16 @@ namespace MyGame
 {
     public class Enemy
     {
+        private string name;
+        private float health;
+        private float shield;
+
+        public Enemy(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
         
     }
 }

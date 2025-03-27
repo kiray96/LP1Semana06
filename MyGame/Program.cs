457309c47ac485822d552b0b342d5990e36ea368
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-03-27 19:46:21 +0000

    Create enemies with user input

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -11,6 +11,17 @@ namespace MyGame
 
             // Criação de array de inimigos
             Enemy[] enemies = new Enemy[emy];
+
+            // Percorrer os indices do array
+            for(int e = 0; e < emy; e++)
+            {
+                // Pedir e guardar o número de inimigos ao utilizador
+                Console.Write($"Nome do inimigo {e + 1}: ");
+                string name = Console.ReadLine();
+
+                // Preencher o array de enemies
+                enemies[e] = new Enemy(name);
+            }
            
             
         }
